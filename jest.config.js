c251e3645d773aebbbb7c35a0a5f4460f75507e3
Author Name <13253042+johanbook@users.noreply.github.com>
2021-07-26 17:07:16 +0200

    test: add min test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 33,
+      lines: 50,
+      statements: 47,
+    },
+  },
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
